c79e2915435dd75505640b61850c796911ae9a1d
David Williams <davidwilliams_canada@yahoo.com>
2018-09-15 06:51:30 +0000

    Created database in admin panel, and added accounts app to settings.py

--- accounts/models.py
+++ accounts/models.py
@@ -2,5 +2,6 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+# Allows us to map our Python code to a database
+# An object-based representation of a database table
 # Create your models here.
--- virtualapp/settings.py
+++ virtualapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-udg$)e-x7)&pg@3ws6nsgr@c&l-5lkc_*_x-i))1!9iwv3tu5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['davsoftware-airesdav333.c9users.io']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
