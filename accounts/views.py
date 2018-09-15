7790cff7c873e7a80c6b01ff01afb82c1d4d05ef
David Williams <davidwilliams_canada@yahoo.com>
2018-09-15 07:32:02 +0000

    Added a route called index.html. Wired up the logout function using the admin panel as test

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
 
 # Create your views here.
+def index(request):
+    """ Return the index.html file (Home) """
+    return render(request, 'index.html')
+    
+def logout(request):
+    """ Log the user out """
+    auth.logout(request)
+    messages.success(request, "YOu have now logged out. See you soon") # a mesage to the user to say they have logged out
+    return redirect(reverse('index'))  # once logged out will return to the index page
+    
--- virtualapp/urls.py
+++ virtualapp/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from accounts.views import index, logout
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', index, name="index"),
+    url(r'^accounts/logout/$', logout, name="logout"),
 ]
