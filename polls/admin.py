3e05a225d033661d863f2c707732d280fc8826f4
David Williams <davidwilliams_canada@yahoo.com>
2018-09-30 10:51:05 +0000

    Registered Poll, Vote, PollSubject to the admin panel so admin can maintain the models for the user

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Poll, Vote, PollSubject
 
-# Register your models here.
+# Register your models to the admin panel so they can displayed to the user
+admin.site.register(Poll)
+admin.site.register(Vote)
+admin.site.register(PollSubject)
