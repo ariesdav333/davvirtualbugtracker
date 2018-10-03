893893864834c99e2905c5173f9f57adf0fede40
David Williams <davidwilliams_canada@yahoo.com>
2018-10-03 17:55:06 +0000

    Added a new cart model

---
+++ cart/contexts.py
@@ -0,0 +1,4 @@
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
+# Will create a session when logged in. This will not be stored as model in the database
--- virtualapp/settings.py
+++ virtualapp/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'threads',
     'products',
     'polls',
+    'cart',
 ]
 
 MIDDLEWARE = [
